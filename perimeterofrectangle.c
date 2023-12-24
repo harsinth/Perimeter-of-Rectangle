bb6323a730563fd6421e0bbe3f72320217e873aa
Nayana <127602515+harsinth@users.noreply.github.com>
2023-12-24 14:08:15 +0530

    Add files via upload

---
+++ perimeterofrectangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int l,b,p;
+    printf("enter the length of rectangle");
+    scanf("%d",&l);
+    printf("enter the breadth of rectangle");
+    scanf("%d",&b);
+    printf("perimeter of rectangle is %d",2*(l+b));
+    return 0;
+}
